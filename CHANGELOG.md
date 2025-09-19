9be8519fa4aab77c0244a5700308c770261cb5c3
dileep-nex <dileep.nexever@gmail.com>
2025-09-19 16:37:25 +0530

    code update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,12 @@
+## 0.0.4
+* Code updated
 
 ## 0.0.3
-
 * Code updated
 
 ## 0.0.2
-
 * formated
 
 ## 0.0.1
-
 * initial release.
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nexever_file_picker/model/return_model.dart';
-
 import 'package:nexever_file_picker/nexever_file_picker.dart';
 
 void main() {
@@ -8,39 +7,98 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final FilePickerHelper filePickerHelper =
-      FilePickerHelper(MyFilePickerState());
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('File Picker Example'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              filePickerHelper.openAttachmentDialog(fileType: "document");
-            },
-            child: Text('Pick Document'),
+          appBar: AppBar(
+            title: Text('File Picker Example'),
           ),
-        ),
+          body: FilePickerScreen()
       ),
     );
   }
 }
 
-class MyFilePickerState extends NexFilePickerState {
+
+class FilePickerScreen extends StatefulWidget {
   @override
-  void success({ReturnModel? fileData, String? type}) {
-    print('File picked: ${fileData!.fileName}');
-    // Implement your success handling here
+  State<FilePickerScreen> createState() => _FilePickerScreenState();
+}
+
+class _FilePickerScreenState extends State<FilePickerScreen>
+    implements NexFilePickerState {
+
+  String _selectedFileName = 'No file selected';
+  String _fileInfo = '';
+
+  late FilePickerHelper filePickerHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    filePickerHelper = FilePickerHelper(this);
   }
 
   @override
-  void error(var error) {
-    print('Error picking file: $error');
-    // Implement your error handling here
+  void success({ReturnModel? fileData, String? type}) {
+    setState(() {
+      _selectedFileName = fileData?.fileName ?? 'Unknown file';
+      _fileInfo = '''
+              File Type: $type
+              File Size: ${(fileData?.size ?? 0) / 1024} KB
+              Extension: ${fileData?.fileExtension ?? 'Unknown'}
+              Path: ${fileData?.filePath ?? 'Unknown'}
+             ''';
+    });
+  }
+
+  @override
+  void error(dynamic error) {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text("File picker"),),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                filePickerHelper.openAttachmentDialog(fileType: "document");
+              },
+              child: const Text('Pick Document'),
+            ),
+            const SizedBox(height: 10),
+            const SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Selected File:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(_selectedFileName),
+                    if (_fileInfo.isNotEmpty) ...[
+                      const SizedBox(height: 10),
+                      Text(_fileInfo),
+                    ],
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
